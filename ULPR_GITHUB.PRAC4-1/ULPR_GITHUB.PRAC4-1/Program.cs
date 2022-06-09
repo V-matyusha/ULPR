6ebac1360c97cdbc821c0c5a6a8717a7c4e837fe
V-matyusha <106967432+V-matyusha@users.noreply.github.com>
2022-06-09 17:45:54 +0700

    Add files via upload

---
+++ ULPR_GITHUB.PRAC4-1/ULPR_GITHUB.PRAC4-1/Program.cs
@@ -0,0 +1,40 @@
+﻿ using System;
+using System.Globalization;
+namespace n1
+{
+    class Program
+    {
+        static void Main()
+        {
+            DateTime date;
+            Console.WriteLine("Введите день:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите месяц:");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите год:");
+            int c = int.Parse(Console.ReadLine());
+            int i;
+            string date2 = $"{c}, {b}, {a}";
+
+            GregorianCalendar calendar = new GregorianCalendar();
+            if (DateTime.TryParse(date2, out date))
+            {
+                Console.WriteLine("День от начала года: " + calendar.GetDayOfYear(date));
+                if (calendar.IsLeapYear(c))
+                {
+                    i = 366 - calendar.GetDayOfYear(date);
+
+                }
+                else
+                {
+                    i = 365 - calendar.GetDayOfYear(date);
+                }
+                Console.WriteLine("Дней до конца года: " + i);
+            }
+            else
+            {
+                Console.WriteLine("Дата не корректна");
+            }
+        }
+    }
+}
