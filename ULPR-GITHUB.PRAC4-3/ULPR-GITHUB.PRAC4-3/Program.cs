cd87fe485b3ed5e249d82457b1cb20c87fed03b7
V-matyusha <106967432+V-matyusha@users.noreply.github.com>
2022-06-09 15:27:18 +0700

    Add files via upload

---
+++ ULPR-GITHUB.PRAC4-3/ULPR-GITHUB.PRAC4-3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+namespace n3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите год: ");
+            int year = int.Parse(Console.ReadLine());
+            for (int month = 1; month < 12; month++)
+            {
+                DateTime date = new DateTime(year, month, DateTime.DaysInMonth(year, month));
+                Console.WriteLine($"{date.Day - (7 + (int)date.DayOfWeek - 4) % 7} число - последний четверг {date.Month}-го месяца {year} года.");
+            }
+        }
+    }
+}
